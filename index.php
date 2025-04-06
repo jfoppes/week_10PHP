e91aad73def8e8f050e5ab1bb8c9c08db65b98ef
Jake <jffoppes@gmail.com>
2025-04-06 18:17:08 -0400

    part 1

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Basic PHP Page</title>
+    </head>
+    <body>
+        <h1><?php echo "Welcome to My Simple PHP Page!"; ?></h1>
+
+    </body>
+</html>
